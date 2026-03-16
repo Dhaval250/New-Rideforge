58e89e79822cede2a5115b0316ff8385beb7747b
Dhaval Panchal <dhavalpanchal250@gmail.com>
2026-03-16 11:47:37 +0530

    new

---
+++ apps/server/src/db/pg.ts
@@ -0,0 +1,10 @@
+﻿import { Pool } from "pg";
+import { env } from "../config/env";
+
+export const pg = new Pool({
+  connectionString: env.databaseUrl,
+});
+
+export const db = {
+  query: <T>(text: string, values?: unknown[]) => pg.query<T>(text, values),
+};
